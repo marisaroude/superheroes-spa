b257310bb363da30d405bb765f03c671082fd1aa
¨marisaroude¨ <marisaroude1@gmail.com>
2024-06-26 23:16:35 -0300

    page add hero

---
+++ src/app/add-hero/page.js
@@ -0,0 +1,11 @@
+'use client'
+import FormHero from '@/components/formHero/FormHero'
+import React from 'react'
+
+export default function AddHero() {
+  return (
+    <div>
+        <FormHero/>
+    </div>
+  )
+}
